55f7e0467b6428b2a82c74090bcec4ed6e446ddf
zicheng yang <143734159+facean2020@users.noreply.github.com>
2026-01-08 16:40:34 +0000

    Not completed

--- Frontend/web-app/src/components/NavigationBar.tsx
+++ Frontend/web-app/src/components/NavigationBar.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const NavigationBar: React.FC = () => {
+
+    const navItems = [
+        { title: 'Home', path: '/'}
+    ];
+
     return (
-        <nav className="navigation-bar">
-            <ul className="nav-links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/features">Features</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
+        <nav className="fixed left-0 right-0 z-50 bg-white dark:bg-gray-950 shadow-md">
+        {/* Bottom navigation: Only displayed on phone */}
+            <div className="mobile-nav md:hidden flex justify-around items-center h-16 border-t border-gray-200 dark:border-gray-700">
+                {navItems.map((item) => (
+                    <Link
+                        key={item.title}
+                        to={item.path}
+                    >
+                        {item.title}
+                    </Link>
+                ))}
+            </div>
         </nav>
     );
 }
